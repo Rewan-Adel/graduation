05117190969fea30ffd11dda07f246aab06af399
Rewan Adel <rewanadel1266@gmail.com>
2023-12-07 21:49:02 +0200

    updates

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -2,7 +2,7 @@ const cloudinary   = require('cloudinary').v2;
 
 cloudinary.config({
     cloud_name : "dmlyrgni5",
-    api_key    :"541583893544472 ",
+    api_key    :"541583893544472",
     api_secret : "PRMJ43cz_zQsBIjOPHt_r5gBlGY",
     secure     : true 
 });
